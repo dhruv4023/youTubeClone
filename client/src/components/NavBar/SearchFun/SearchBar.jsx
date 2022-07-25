350f882b75b30d416a6b979214a0c842f62b27c7
Dhruv <dhruv20345@gmail.com>
2022-07-25 16:54:58 +0530

    search fun

--- client/src/components/NavBar/SearchFun/SearchBar.jsx
+++ client/src/components/NavBar/SearchFun/SearchBar.jsx
@@ -1,89 +1,68 @@
 import React from "react";
 
 import { FaSearch } from "react-icons/fa";
-import { BsMicFill } from "react-icons/bs";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-
+import SearchBtns from "./SearchBtns";
 import "./searchBar.css";
-import SearchList from "./SearchList";
-
 function SearchBar() {
-  const [searchMobile, setSearchMobile] = useState({ display: "none" });
+  const [searchMobile, setSearchMobile] = useState({
+    mobile: { display: "none" },
+    // web: { display: "flex" },
+  });
   const searchMobileToggle = () => {
-    if (searchMobile.display === "none") {
+    console.log(searchMobile.mobile);
+    if (searchMobile.mobile.display === "none") {
       setSearchMobile({
-        display: "block",
+        mobile: { display: "block" },
       });
     } else {
       setSearchMobile({
-        display: "none",
+        mobile: { display: "none" },
       });
     }
   };
-
-  const [searchQuery, setSearchQuery] = useState("");
-
-  // const value = searchQuery;
-  // console.log(searchQuery);
-  const TitleArray = useSelector((state) => state.videoReducer)
-    ?.data?.filter(
-      (q) => q?.videoTitle.toUpperCase().includes(searchQuery.toUpperCase())
-      // console.log(q)
-    )
-    .map((m) => m.videoTitle);
 
   // console.log(searchQuery);
   // console.log(TitleArray);
 
   return (
     <>
-      <div className="search_div">
-        <div className="search_div2">
-          <input
-            type="text"
-            className="Search_Navbar"
-            placeholder="Search"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <Link to={`/search/${searchQuery}`}>
-            <FaSearch className="searchIcon_Navbar" />
-          </Link>
-          <BsMicFill size={20} className="mic_Navbar" />
-          {searchQuery && (
-            <SearchList
-              className="search"
-              titleList={TitleArray}
-              setSearchQuery={setSearchQuery}
-            />
-          )}
+      <div className="webView">
+        <div className="search_div">
+            <SearchBtns /> 
         </div>
       </div>
-      <div className="searchBar_Navbar_Mobile Mobile" style={searchMobile}>
+      {/* <div
+        className="searchBar_Navbar_Mobile Mobile"
+        style={searchMobile.mobile}
+      >
+        <SearchBtns />
+        <div className="X_SEacrbar_mobile Mobile" onClick={searchMobileToggle}>
+          X
+        </div>
+      </div> */}
+      {/*
         <div className="search_div">
           <input
             type="text"
             className="Mobile Search_Navbar"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
+            onClick={(e)=>setSearchList(true)}
             placeholder="Search"
           />
           <Link to={`/search/${searchQuery}`}>
-            <FaSearch className="searchIcon_Navbar Mobile" />
+            <FaSearch onClick={(e)=>setSearchList(false)}  className="searchIcon_Navbar Mobile" />
           </Link>
           <BsMicFill size={20} className="mic_Navbar Mobile" />
-          <div className="X_SEacrbar_mobile" onClick={searchMobileToggle}>
-            X
-          </div>
+
         </div>
-        {searchQuery && (
+        {searchList&& searchQuery && (
           <SearchList
           className="Mobile"
           titleList={TitleArray} setSearchQuery={setSearchQuery} />
         )}
-      </div>
+      */}
       <FaSearch
         className="vid_bell_Navbar Mobile"
         size={20}
---
+++ client/src/components/NavBar/SearchFun/SearchBtns.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+import { BsMicFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import SearchList from "./SearchList";
+import "./searchBar.css";
+
+import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+
+function SearchBtns({ searchMobileToggle }) {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchList, setSearchList] = useState(false);
+
+  // const value = searchQuery;
+  // console.log(searchQuery);
+  const TitleArray = useSelector((state) => state.videoReducer)
+    ?.data?.filter(
+      (q) => q?.videoTitle.toUpperCase().includes(searchQuery.toUpperCase())
+      // console.log(q)
+    )
+    .map((m) => m.videoTitle);
+  return (
+    <>
+    
+    <div className="search_div2">
+      <input
+        type="text"
+        className="Search_Navbar"
+        placeholder="Search"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        onClick={(e) => setSearchList(true)}
+      />
+      <Link to={`/search/${searchQuery}`}>
+        <FaSearch
+          onClick={(e) => setSearchList(false)}
+          className="searchIcon_Navbar"
+        />
+      </Link>
+      <BsMicFill size={20} className="mic_Navbar" />
+      {searchQuery && searchList && (
+        <SearchList
+          className="search"
+          titleList={TitleArray}
+          setSearchQuery={setSearchQuery}
+        />
+      )}
+      </div>
+    </>
+  );
+}
+
+export default SearchBtns;
